1b3d1a4ef12e3cbaea53a96060c06b578df3718b
immprada <im.mprada@gmail.com>
2022-09-13 18:11:21 -0500

    Add class LinkedList

--- lib/ds/linked_list.rb
+++ lib/ds/linked_list.rb
@@ -1,8 +1,25 @@
 module Ds
   class LinkedList
-
     def initialize
-      Node.new
+      @head = nil
+      @tail = nil
+    end
+
+    def add(number)
+      if @head.nil?
+        @head = Node.new(number)
+        @tail = @head
+      else
+        @tail.next_node = Node.new(number)
+        @tail = @tail.next_node
+      end
+    end
+
+    def get(index)
+      current_node = @head
+      (0...index).each { current_node = current_node.next_node }
+
+      current_node.value
     end
   end
 
