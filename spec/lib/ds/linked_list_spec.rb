5f0863758b66e5e8d0593acdd7a374b0760f18b5
immprada <im.mprada@gmail.com>
2022-09-13 18:16:04 -0500

    Add Node spec

--- spec/lib/ds/linked_list_spec.rb
+++ spec/lib/ds/linked_list_spec.rb
@@ -2,5 +2,31 @@ require './spec/spec_helper'
 
 RSpec.describe LinkedList do
   describe 'class Node' do
+    let(:node) { Node.new(1) }
+    let(:another_node) { Node.new(2, node) }
+
+    it 'returns a Node object' do
+      expect(node).to be_a Node
+    end
+
+    describe 'when initialized with only a value' do
+      it 'has a value' do
+        expect(node.value).to eq 1
+      end
+
+      it 'has a nil next_node' do
+        expect(node.next_node).to be_nil
+      end
+    end
+
+    describe 'when initialized with a value and a next_node' do
+      it 'has a value' do
+        expect(another_node.value).to eq 2
+      end
+
+      it 'has a next_node' do
+        expect(another_node.next_node).not_to be_nil
+      end
+    end
   end
 end
