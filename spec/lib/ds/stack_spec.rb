f83430ebb153a77937baaba28f3a30db836e0b80
immprada <im.mprada@gmail.com>
2022-09-16 15:05:54 -0500

    specs for #pop

--- spec/lib/ds/stack_spec.rb
+++ spec/lib/ds/stack_spec.rb
@@ -19,4 +19,34 @@ RSpec.describe Stack do
       expect(stack.empty?).to be false
     end
   end
+
+  describe '#pop' do
+    let(:last_element) { 56 }
+
+    before do
+      stack.push(1)
+      stack.push(2)
+      stack.push(last_element)
+    end
+
+    it 'returns removed element' do
+      expect(stack.pop).to be(last_element)
+    end
+
+    describe 'when #pop empties the stack' do
+      before do
+        stack.pop
+        stack.pop
+        stack.pop
+      end
+
+      it 'is empty' do
+        expect(stack.empty?).to be true
+      end
+
+      it 'returns nil' do
+        expect(stack.pop).to be_nil
+      end
+    end
+  end
 end
