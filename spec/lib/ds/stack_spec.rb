8f92ba34c5ee6070c7cdff639fb646c9b742bc53
immprada <im.mprada@gmail.com>
2022-09-16 14:48:20 -0500

    add specs "when instantiated"

--- spec/lib/ds/stack_spec.rb
+++ spec/lib/ds/stack_spec.rb
@@ -3,4 +3,13 @@ require './spec/spec_helper'
 RSpec.describe Stack do
   let(:stack) { described_class.new }
 
+  describe 'when instantiated' do
+    it 'creates a new stack' do
+      expect(stack).to be_a(described_class)
+    end
+
+    it 'is empty' do
+      expect(stack.empty?).to be true
+    end
+  end
 end
