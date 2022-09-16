ff3d755d153b4b1682f2364c39bd99c46f948e81
immprada <im.mprada@gmail.com>
2022-09-16 14:43:53 -0500

    Add Stack class with #push and #pop

--- lib/scripts/stack.rb
+++ lib/scripts/stack.rb
@@ -1,5 +1,15 @@
 require_relative 'linked_list'
 
 class Stack
+  def initialize
+    @stack = LinkedList.new
+  end
 
+  def push(value)
+    @stack.add_at(0, value)
+  end
+
+  def pop
+    @stack.remove(0)
+  end
 end
