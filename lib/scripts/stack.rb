78f43cbc034a43636c5f9e0e6982b20cb8056a1a
immprada <im.mprada@gmail.com>
2022-09-17 22:47:05 -0500

    add @min and #update_min_by_push to Stack

--- lib/scripts/stack.rb
+++ lib/scripts/stack.rb
@@ -2,11 +2,13 @@ class Stack
   def initialize
     @stack = LinkedList::SingleLinkedList.new
     @size = 0
+    @min = nil
   end
 
   def push(value)
     @size += 1
     @stack.add_at(0, value)
+    update_min_by_push(value)
   end
 
   def pop
@@ -16,5 +18,13 @@ class Stack
 
   def empty?
     @size.zero?
+  end
+
+  private
+
+  def update_min_by_push(value)
+    return @min = value if @min.nil?
+
+    @min = value if value < @min
   end
 end
---
+++ lib/stack_min.rb
@@ -0,0 +1,21 @@
+require_relative 'main'
+
+stack = Stack.new
+
+stack.push(3)
+stack.push(5)
+puts stack.min
+# => 3
+
+stack.pop
+stack.push(7)
+puts stack.min
+# => 3
+
+stack.push(2)
+puts stack.min
+# => 2
+
+stack.pop
+puts stack.min
+# => 3
